30a93d7d7acdfbe27684e286f433fa47e72fa7ee
Eugene Amagyei <eugeneprahamagyei@gmail.com>
2022-12-15 05:53:35 +0000

    Some Mandatory Done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - code entry
+ *
+ * Return: empty
+ *
+ */
+
+void print_alphabet(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; ++alpha)
+{
+	_putchar(alpha);
+}
+_putchar('\n');
+}
