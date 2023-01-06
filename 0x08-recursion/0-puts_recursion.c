016515fe8f934f7bea7bc4f2955cb1352d84e49f
Eugene Amagyei <eugeneprahamagyei@gmail.com>
2023-01-06 04:24:38 +0000

    Part of Mandatory Done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - code entry
+ * @s: string
+ *
+ * Return: code status
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+	_putchar('\n');
+else
+{
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
+}
