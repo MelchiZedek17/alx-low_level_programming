251acd7f165945a410e0743328154ce81f223769
Eugene Amagyei <eugeneprahamagyei@gmail.com>
2023-02-03 02:26:01 +0000

    Mandatory Tasks Done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,10 +6,11 @@
  *
  * Return:converted number, or 0
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 int i;
-unsigned int num = 0;
+unsigned int dec_val = 0;
 
 if (!b)
 	return (0);
@@ -17,7 +18,7 @@ for (i = 0; b[i]; i++)
 {
 	if (b[i] < '0' || b[i] > '1')
 		return (0);
-	num = 2 * num + (b[i] - '0');
+	dec_val = 2 * dec_val + (b[i] - '0');
 }
-return (num);
+return (dec_val);
 }
